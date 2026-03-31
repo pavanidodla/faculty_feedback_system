9534e0d522057d80b60b55e62264c5e5adb08069
pavanidodla <dodlapavani511@gmail.com>
2026-03-31 09:15:24 +0530

    Update Register.js

--- faculty-feedback-frontend/src/pages/Register.js
+++ faculty-feedback-frontend/src/pages/Register.js
@@ -25,7 +25,7 @@ export default function Register() {
       );
     }
 
-    const res = await API.post("/auth/register", {
+    const res = await API.post("/api/auth/register", {
       name,
       email,
       password,
