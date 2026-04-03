05433e87065fc4aff794139a4229c3813dbaa334
pavanidodla <dodlapavani511@gmail.com>
2026-04-03 11:45:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/authRoutes.js";
 import feedbackRoutes from "./routes/feedbackRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
